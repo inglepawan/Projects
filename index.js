1ff20996d1fa26dd85f43fd8def08cbacaf7c038
code-with-PawanIngle <pawaningle.corporate@gmail.com>
2025-01-24 21:16:41 +0530

    new project-countdown

---
+++ index.js
@@ -0,0 +1,28 @@
+const daysEl =document.getElementById("days");
+const hoursEl =document.getElementById("hours");
+const minutesEl =document.getElementById("minutes");
+const secondsEl =document.getElementById("seconds");
+
+const endDate = "28 Feb 2025";
+
+function countdown(){
+    const conclusionDate = new Date(endDate);
+    const currentDate = new Date();
+
+    const totalSeconds = (conclusionDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = hours;
+    minutesEl.innerHTML = minutes;
+    secondsEl.innerHTML = seconds;
+
+}
+
+countdown();
+
+setInterval(countdown, 1000);
