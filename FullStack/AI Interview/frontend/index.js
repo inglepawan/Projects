878b046bdc3771964ee88c6b8b5408d705be480c
code-with-PawanIngle <pawaningle.corporate@gmail.com>
2025-01-26 22:43:06 +0530

    new project

---
+++ FullStack/AI Interview/frontend/index.js
@@ -0,0 +1,12 @@
+// function toggleMode() {
+//     // Toggle the dark-mode class on the body element
+//     document.body.classList.toggle("dark-mode");
+
+//     // Change the button text/icon accordingly
+//     const toggleButton = document.querySelector(".toggle span");
+//     if (document.body.classList.contains("dark-mode")) {
+//         toggleButton.innerHTML = "🌕";  // Change to sun icon for dark mode
+//     } else {
+//         toggleButton.innerHTML = "🌙";  // Change to moon icon for light mode
+//     }
+// }
