29de894867ed0212529e40d3b207f229c399202c
Pawan Ingle <pawaningle.corporate@gmail.com>
2025-05-22 14:01:20 +0530

    Update index.js

--- FrontEnd/Countdown-Timer/index.js
+++ FrontEnd/Countdown-Timer/index.js
@@ -3,7 +3,7 @@ const hoursEl =document.getElementById("hours");
 const minutesEl =document.getElementById("minutes");
 const secondsEl =document.getElementById("seconds");
 
-const endDate = "28 Feb 2025";
+const endDate = "30 Sep 2025";
 
 function countdown(){
     const conclusionDate = new Date(endDate);
